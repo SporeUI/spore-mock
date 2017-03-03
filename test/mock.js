639f6cb8f8b072d32d0609e4ee55be72d7465b22
Tony.Liang <pillar0514@163.com>
2017-03-03 15:50:23 +0800

    添加测试范例文件

--- test/mock.js
+++ test/mock.js
@@ -6,12 +6,12 @@ module.exports = {
 	// 链接列表，用于显示到首页
 	links: [
 		{
-			href: 'http://127.0.0.1:8091',
-			text: 'Mock服务 http://127.0.0.1:8091'
+			href: 'http://{{publicIp}}:8091',
+			text: 'Mock服务 http://{{publicIp}}:8091'
 		},
 		{
-			href: 'http://127.0.0.1:8092',
-			text: '代理服务 http://127.0.0.1:8092'
+			href: 'http://{{publicIp}}:8092',
+			text: '代理服务 http://{{publicIp}}:8092'
 		}
 	],
 	// 二维码链接列表，用于显示到 mock 服务首页
---
+++ test/src/mock/api/test.js
@@ -0,0 +1,5 @@
+module.exports = {
+	ret: 0,
+	msg: 'test ok'
+};
+

4f712ed11fe3b1fe30c35a85a666d23b69985423
Tony.Liang <pillar0514@163.com>
2017-05-02 12:04:09 +0800

    更新样式

--- test/mock.js
+++ test/mock.js
@@ -7,17 +7,17 @@ module.exports = {
 	links: [
 		{
 			href: 'http://{{publicIp}}:8091',
-			text: 'Mock服务 http://{{publicIp}}:8091'
+			text: 'mock: http://{{publicIp}}:8091'
 		},
 		{
 			href: 'http://{{publicIp}}:8092',
-			text: '代理服务 http://{{publicIp}}:8092'
+			text: 'proxy: http://{{publicIp}}:8092'
 		}
 	],
 	// 二维码链接列表，用于显示到 mock 服务首页
 	qrlinks: [{
 		href: 'http://{{publicIp}}:8090',
-		text: '开发服务 http://{{publicIp}}:8090'
+		text: 'dev: http://{{publicIp}}:8090'
 	}],
 	// 指定静态文件路径
 	statics: [
